30516ab821beef225de8dd43aec7a4d0b4a85d5b
gustavofurlani <142332077+gustavofurlani@users.noreply.github.com>
2025-02-21 19:46:12 -0300

    Create lista2\ex2.py

---
+++ lista2\ex2.py
@@ -0,0 +1,9 @@
+n1 = int(input("digite um numero:"))
+n2 = int(input("digite outro numero:"))
+
+if n1 > n2:
+    print(F"{n1} e o maior numero")
+elif n2 > n1:
+    print(f"{n2} e maior")
+else :
+    print(f"{n1} e {n2} sao iguais")
