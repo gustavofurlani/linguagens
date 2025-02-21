7c7b8765abbb1e8d995fa324e5390162412b1efa
gustavofurlani <142332077+gustavofurlani@users.noreply.github.com>
2025-02-21 19:34:08 -0300

    Create lista1\ex4.py

---
+++ lista1\ex4.py
@@ -0,0 +1,12 @@
+horasTrab = float(input("digite as horas trabalhadas:"))
+valorHora = float(input("valor da hora:"))
+
+salario = horasTrab * valorHora
+
+print(f"salario {salario:.2f}")
+
+
+metros = float(input("digite o valor em metros:"))
+
+cent = metros * 100
+print(f"valor em cetimetros: {cent:.2f}")
