dde1f4f9539ca8d147bb2d93c694e149716a2def
gustavofurlani <142332077+gustavofurlani@users.noreply.github.com>
2025-02-21 19:57:38 -0300

    Create lista3\ex2.py

---
+++ lista3\ex2.py
@@ -0,0 +1,9 @@
+soma = 0
+
+while True:
+    num = float(input("Digite um número (negativo para sair): "))
+    if num < 0:
+        break
+    soma += num
+
+print(f"A soma dos números positivos é: {soma}")
